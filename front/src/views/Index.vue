0a871a7609569dea5ec617ec8dfe0ef94e1b4b82
tkawamura <tkawamura.stayhome@gmail.com>
2021-05-31 20:33:59 +0900

    before heroku deploy

---
+++ front/src/views/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="index">
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg() {
+      axios.get('http://localhost:3000/api/v1/hello')
+        .then(res => this.msgs.push(res.data))
+    }
+  }
+};
+</script>
